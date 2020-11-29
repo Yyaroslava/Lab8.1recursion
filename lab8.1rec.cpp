16bdc17adc500e51ad99fe3f65415a562295e9e0
Yaroslava <72552637+Yyaroslava@users.noreply.github.com>
2020-11-29 17:00:52 +0200

    Add files via upload

--- lab8.1rec.cpp
+++ lab8.1rec.cpp
@@ -8,7 +8,14 @@
 
 using namespace std;
 
-void Process_r(char* str, char* newStr, bool &changed, char &state, int &strPos, int &newStrPos) {
+// c - previous character
+bool Find_r(char* str, char c) {
+    if (str[0] == 0) return false;
+    if (str[0] == c) return true;
+    return Find_r(str + 1, str[0]);
+}
+
+void Process_r(char* str, char* newStr, char &state, int &strPos, int &newStrPos) {
     if (str[strPos] == 0) {
         if (state == 'a') newStr[newStrPos++] = 'a';
         if (state == 'b') newStr[newStrPos++] = 'b';
@@ -23,7 +30,6 @@ void Process_r(char* str, char* newStr, bool &changed, char &state, int &strPos,
             newStr[newStrPos++] = '*';
             newStr[newStrPos++] = '*';
             newStr[newStrPos++] = '*';
-            changed = true;
             state = ' ';
         }
         else {
@@ -37,7 +43,6 @@ void Process_r(char* str, char* newStr, bool &changed, char &state, int &strPos,
             newStr[newStrPos++] = '*';
             newStr[newStrPos++] = '*';
             newStr[newStrPos++] = '*';
-            changed = true;
             state = ' ';
         }
         else {
@@ -51,7 +56,6 @@ void Process_r(char* str, char* newStr, bool &changed, char &state, int &strPos,
             newStr[newStrPos++] = '*';
             newStr[newStrPos++] = '*';
             newStr[newStrPos++] = '*';
-            changed = true;
             state = ' ';
         }
         else {
@@ -70,23 +74,24 @@ void Process_r(char* str, char* newStr, bool &changed, char &state, int &strPos,
     }
     strPos++;
 
-    Process_r(str, newStr, changed, state, strPos, newStrPos);
+    Process_r(str, newStr, state, strPos, newStrPos);
     return;
 }
 
 int main() {
     char str[101];
     char newStr[151];
-    bool changed = false;
 
     cout << "Enter string:" << endl;
     cin.getline(str, 100);
+
+    bool changed = Find_r(str,  ' ');
 
     char state = ' ';
     int strPos = 0;
     int newStrPos = 0;
     
-    Process_r(str, newStr, changed, state, strPos, newStrPos);
+    Process_r(str, newStr, state, strPos, newStrPos);
 
     cout << "String: " << str << endl << endl;
     if (changed) {
